08371b1e09e44d5b010240298603c09c998d4ad9
findingme <63691250+findingme@users.noreply.github.com>
2023-02-24 17:23:35 +0900

    Add files via upload

---
+++ background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessage.addListener((request) => {
+
+  // 期待通りのリクエストかどうかをチェック
+  if (request.name === 'displayUrl:background') {
+    let url;
+    let id;
+
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      console.log({ tabs })
+      url = tabs[0].url;
+      id = tabs[0].id
+
+      // content_script へデータを送る
+      chrome.tabs.sendMessage(id, { // content_script はタブごとに存在するため ID 指定する必要がある
+        name: 'displayUrl:contentScripts',
+        data: {
+          url
+        }
+      })
+    });
+  }
+});
---
+++ content.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener((request, options) => {
+  if (request.name === 'displayUrl:contentScripts') {
+    const body = document.querySelector('body')
+    const addElement = document.createElement('h1');
+
+    // 受け取った URL を画面に表示する
+    addElement.textContent = `URL is ${ request.data.url }`;
+
+    body.prepend(addElement)
+  }
+});
